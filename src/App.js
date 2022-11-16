8e7d0a4b2462c3d4da4af3eda33e1c2a719b4a44
Christian Nielsen <chrnielsen2003@gmail.com>
2022-11-16 12:57:47 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,19 +1,50 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 function App() {
   const [location, setLocation] = useState({});
-
+  const [weather, setWeather] = useState({});
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
-      console.log(position);
       setLocation(position);
     });
+
+    axios
+      .get(
+        `https://api.open-meteo.com/v1/forecast?latitude=55.62&longitude=12.08&hourly=temperature_2m`
+      )
+      .then((e) => {
+        setWeather(e.data);
+      });
   }, []);
 
   return (
     <div className="App">
       <h1>Hello World</h1>
       <p>{location.timestamp}</p>
+      <p>Latitude: {location?.coords?.latitude}</p>
+      <p>Longitude: {location?.coords?.longitude}</p>
+
+      <div>
+        <div>
+          {weather?.hourly?.time.map((data) => {
+            const splitDate = data.split("-");
+            const datoArray = splitDate[2].split("T");
+            const dato = datoArray[0];
+            const time = datoArray[1];
+            console.log(time);
+            return (
+              <>
+                <div>
+                  <p>
+                    {time} Den {dato}.
+                  </p>
+                </div>
+              </>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
