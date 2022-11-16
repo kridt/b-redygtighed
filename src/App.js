6728931f0e6d332b9c1b258ecc5d64900ff34ebb
Christian Nielsen <chrnielsen2003@gmail.com>
2022-11-16 13:33:21 +0100

    weather

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import { useEffect, useState } from "react";
 function App() {
   const [location, setLocation] = useState({});
   const [weather, setWeather] = useState({});
+  const nu = new Date().getHours();
+
+  console.log(nu);
+
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
       setLocation(position);
@@ -11,37 +15,48 @@ function App() {
 
     axios
       .get(
-        `https://api.open-meteo.com/v1/forecast?latitude=55.62&longitude=12.08&hourly=temperature_2m`
+        `https://api.open-meteo.com/v1/forecast?latitude=55.62&longitude=12.08&hourly=temperature_2m,rain&timezone=Europe%2FBerlin`
       )
       .then((e) => {
         setWeather(e.data);
+        console.log(weather);
       });
   }, []);
 
   return (
     <div className="App">
       <h1>Hello World</h1>
-      <p>{location.timestamp}</p>
-      <p>Latitude: {location?.coords?.latitude}</p>
-      <p>Longitude: {location?.coords?.longitude}</p>
 
-      <div>
+      <div style={{ textAlign: "center" }}>
+        <h2>Lige nu</h2>
+        <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+          <h2>Time: {nu}</h2>
+          <h2>Grader: {weather?.hourly.temperature_2m[nu]}</h2>
+        </div>
+      </div>
+
+      <div style={{ display: "flex", justifyContent: "space-evenly" }}>
         <div>
           {weather?.hourly?.time.map((data) => {
             const splitDate = data.split("-");
             const datoArray = splitDate[2].split("T");
             const dato = datoArray[0];
             const time = datoArray[1];
-            console.log(time);
+
             return (
               <>
-                <div>
+                <div key={time}>
                   <p>
-                    {time} Den {dato}.
+                    kl {time} Den {dato}. er det
                   </p>
                 </div>
               </>
             );
+          })}
+        </div>
+        <div>
+          {weather?.hourly?.temperature_2m?.map((data) => {
+            return <p> {data} °C</p>;
           })}
         </div>
       </div>
